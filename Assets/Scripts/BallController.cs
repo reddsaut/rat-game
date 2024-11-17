d2202fbbaff124dcab510c5f75d6351aaf0194e6
reddsaut <osauter@macalester.edu>
2024-11-17 12:22:34 +0100

    ported over ball rolling lab
    will modify later to suit our needs

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public Transform ballCamera;
+
+    public float acceleration = 10;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        float moveLeftRight = Input.GetAxis ("Horizontal");
+        float moveForwardBack = Input.GetAxis ("Vertical");
+
+        Vector3 xAcceleration = ballCamera.right * moveLeftRight * Time.deltaTime * acceleration;
+
+        Vector3 forward = Vector3.ProjectOnPlane(ballCamera.forward, Vector3.up);
+        forward.Normalize();
+
+        Vector3 zAcceleration = forward * moveForwardBack * Time.deltaTime * acceleration;
+
+        rb.linearVelocity += xAcceleration + zAcceleration;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="CameraController.cs" company="DIS Copenhagen">
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public LayerMask obstacleLayerMask;
+
+    public float distance = 10;
+    public float minPitch = -80;
+    public float maxPitch = 80;
+
+    public float rotationSpeed = 300;
+
+    private float pitch;
+    private float yaw;
+
+    void Start()
+    {
+        pitch = 45;
+        yaw = 0;
+    }
+
+    void Update()
+    {
+        float pitchInput = Input.GetAxisRaw("Mouse Y");
+        pitch += pitchInput * Time.deltaTime * rotationSpeed;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        Quaternion pitchRotation = Quaternion.AngleAxis(pitch, Vector3.right);
+
+        float yawInput = Input.GetAxisRaw("Mouse X");
+        yaw += yawInput * Time.deltaTime * rotationSpeed;
+        Quaternion yawRotation = Quaternion.AngleAxis(yaw, Vector3.up);
+
+        Vector3 offset = new Vector3(0,1,-1);
+        offset = pitchRotation * offset;
+        offset = yawRotation * offset;
+
+        transform.position = target.position + offset * distance;
+        transform.rotation = Quaternion.LookRotation(-offset, Vector3.up);
+    }
+
+}

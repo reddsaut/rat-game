57c0a13a32a0b1518e05a5cfd93eda2b3bf562e9
Ben Snyder <bsnyder2@users.noreply.github.com>
2024-11-26 12:57:36 +0100

    Man textures and controller

---
+++ Assets/Scripts/ManGenerator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ManGenerator : MonoBehaviour
+{
+    private const float interManDistance = 10;
+    private GameObject man;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for (int i = 0; i < 9; i++)
+        //{
+        //    Instantiate(man, transform.position);
+        //}
+    }
+}

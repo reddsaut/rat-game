daa0a63f8c4f9825c4167c1c879958abdc8a4c40
reddsaut <osauter@macalester.edu>
2024-12-08 13:35:17 +0100

    main menu done

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -6,18 +6,36 @@ public class MenuManager : MonoBehaviour
     public GameObject mainMenu;
     public GameObject introMenu;
     public GameObject controlsScreen;
+    public GameObject creditsScreen;
+    GameObject currentScreen;
 
     void Start()
     {
         mainMenu.SetActive(true);
         introMenu.SetActive(false);
         controlsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        currentScreen = mainMenu;
     }
 
-    public void ToIntroMenu()
+    void Update()
     {
-        mainMenu.SetActive(false);
-        introMenu.SetActive(true);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            currentScreen.SetActive(false);
+            mainMenu.SetActive(true);
+            currentScreen = mainMenu;
+        }
+    }
+
+    public void ToggleIntroMenu()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        introMenu.SetActive(!introMenu.activeSelf);
+        if(introMenu.activeSelf)
+        {
+            currentScreen = introMenu;
+        }
     }
 
     public void StartGame()
@@ -29,5 +47,19 @@ public class MenuManager : MonoBehaviour
     {
         mainMenu.SetActive(!mainMenu.activeSelf);
         controlsScreen.SetActive(!controlsScreen.activeSelf);
+        if(controlsScreen.activeSelf)
+        {
+            currentScreen = controlsScreen;
+        }
+    }
+
+    public void ToggleCredits()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        creditsScreen.SetActive(!controlsScreen.activeSelf);
+        if(creditsScreen.activeSelf)
+        {
+            currentScreen = creditsScreen;
+        }
     }
 }
